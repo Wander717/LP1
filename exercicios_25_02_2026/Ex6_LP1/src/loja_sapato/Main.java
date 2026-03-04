e18ab45d69e16306344257dd83ccbe01fd136259
Wander717 <wandersonrsantos01@gmail.com>
2026-03-04 15:56:01 -0300

    Adicionado o exercício 6.

---
+++ exercicios_25_02_2026/Ex6_LP1/src/loja_sapato/Main.java
@@ -0,0 +1,14 @@
+package loja_sapato;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Sapato tenis = new Sapato ("Nike", "Preto", 43, 2);
+		
+		tenis.Informacoes();
+		
+		
+
+	}
+
+}
