5f1d0185f43dd6b9c8c0e42881783b9f0dd05838
Wander717 <wandercpv07@gmail.com>
2026-05-15 20:25:08 -0300

    feat(Entidades): Entidades preenchidas.

--- TELAS_FUNCIONAIS_LP1/tela_salao/src/main/java/org/example/tela_salao/entidades/Funcionario.java
+++ TELAS_FUNCIONAIS_LP1/tela_salao/src/main/java/org/example/tela_salao/entidades/Funcionario.java
@@ -1,4 +1,35 @@
 package org.example.tela_salao.entidades;
 
 public class Funcionario {
+    private int id;
+    private String nome;
+    private String sexo;
+    private String cargo;
+    private int idade;
+
+    //CONSTRUTOR
+    public Funcionario(int id, String nome, String sexo, String cargo, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.cargo = cargo;
+        this.idade = idade;
+    }
+
+    //CONSTRUTOR VAZIO
+    public Funcionario() {}
+
+    //GETTERS
+    public int getId() {return id;}
+    public String getNome() {return nome;}
+    public String getSexo() {return sexo;}
+    public String getCargo() {return cargo;}
+    public int getIdade() {return idade;}
+
+    //SETTERS
+    public void setId(int id) {this.id = id;}
+    public void setNome(String nome) {this.nome = nome;}
+    public void setSexo(String sexo) {this.sexo = sexo;}
+    public void setCargo(String cargo) {this.cargo = cargo;}
+    public void setIdade(int idade) {this.idade = idade;}
 }
--- TELAS_FUNCIONAIS_LP1/tela_salao/src/main/java/org/example/tela_salao/entidades/Produto.java
+++ TELAS_FUNCIONAIS_LP1/tela_salao/src/main/java/org/example/tela_salao/entidades/Produto.java
@@ -1,4 +1,32 @@
 package org.example.tela_salao.entidades;
 
 public class Produto {
+    // Atributos baseados nas colunas da tabela 'produto'
+    private int id;
+    private String nome;
+    private String tipo;
+    private int quantidade;
+
+    //CONSTRUTOR
+    public Produto(int id, String nome, String tipo, int quantidade) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+    }
+
+    //CONSTRUTOR VAZIO
+    public Produto() {}
+
+    //GETTERS
+    public int getId() {return id;}
+    public String getNome() {return nome;}
+    public String getTipo() {return tipo;}
+    public int getQuantidade() {return quantidade;}
+
+    //SETTERS
+    public void setId(int id) {this.id = id;}
+    public void setNome(String nome) {this.nome = nome;}
+    public void setTipo(String tipo) {this.tipo = tipo;}
+    public void setQuantidade(int quantidade) {this.quantidade = quantidade;}
 }
